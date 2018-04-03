3c960fd09a4147a3c712c7653c4827e577d4c610
Zhengxiang Gong <jessiegong92@gmail.com>
2018-04-03 03:05:35 +0000

    automatically backed up by learn

--- lib/book.rb
+++ lib/book.rb
@@ -2,9 +2,15 @@ class Book
   attr_accessor :author, :page_count
   attr_reader :title, :genre
 
+  GENRE = []
+
   def initialize(title)
     @title = title
   end
+
+  def genre=(genre)
+      @genre = genre
+      GENRE << @genre
 
   def turn_page
     puts "Flipping the page...wow, you read fast!"
